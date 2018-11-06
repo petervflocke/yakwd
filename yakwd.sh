17a08f5d5ad7d482db0ed142ef38990389b54a07
Piotr <peter.von.flocke@gmail.com>
2018-11-06 17:56:48 +0100

    beta v0.2

--- yakwd.sh
+++ yakwd.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+SERVICE=/usr/local/bin/yakwd
+if pgrep -f "$SERVICE" >/dev/null 2>&1 ; then
+  echo "$SERVICE is RUNNING"
+else
 /etc/init.d/framework stop
 /usr/bin/lipc-set-prop -- com.lab126.powerd preventScreenSaver 1
 /usr/sbin/eips -s 5
 /usr/local/bin/yakwd
 /usr/bin/lipc-set-prop -- com.lab126.powerd preventScreenSaver 0
 /etc/init.d/framework start
+fi
