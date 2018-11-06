63a2311a3e7e5a21267f4043be0e3bc2f153c25f
Piotr <peter.von.flocke@gmail.com>
2018-11-06 11:54:49 +0100

    cleanup

--- yakwd.sh
+++ yakwd.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+#!/bin/sh
 /etc/init.d/framework stop
 /usr/bin/lipc-set-prop -- com.lab126.powerd preventScreenSaver 1
+/usr/sbin/eips -s 5
 ./yakwd
 /usr/bin/lipc-set-prop -- com.lab126.powerd preventScreenSaver 0
 /etc/init.d/framework start
